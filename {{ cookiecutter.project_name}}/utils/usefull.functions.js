e3b98721c4e346f10482c8a4fddd2048b576f717
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-12-18 20:31:38 +0330

    Update usefull.functions.js

--- {{ cookiecutter.project_name}}/utils/usefull.functions.js
+++ {{ cookiecutter.project_name}}/utils/usefull.functions.js
@@ -1,9 +1,20 @@
 "use strict";
-
+const fs = require("fs");
 module.exports = {
     LOAD_CONFIG: () => {
         Object.entries(require('../config')).forEach(entry => {
             const [key0, value0] = entry;
+            fs.readFile("../.env", (err, data) => {
+                if (err) return;
+                let arr = data.toString().split("\n");
+                arr.forEach((element) => {
+                    key_value = element.split("=");
+                    if (key_value[0] && key_value[1]) {
+                        let value = require("../config")[key0][key_value[0]]
+                        if ( value ) require("../config")[key0][key_value[0]] = key_value[1]
+                    }
+                });
+            });
             Object.entries(value0).forEach(entry => {
                 const [key1, value1] = entry;
                 if (process.env[key1] !== undefined) {
