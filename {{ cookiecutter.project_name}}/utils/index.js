0228aa09a2c04f5e7352a074a45b20ba154ac4d8
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-12-19 18:30:07 +0330

    Update index.js

--- {{ cookiecutter.project_name}}/utils/index.js
+++ {{ cookiecutter.project_name}}/utils/index.js
@@ -2,5 +2,12 @@
 
 
 module.exports = {
-    usefull_functions: require('./usefull.functions')
+    usefull_functions: require("./usefull.functions"),
+    GET_PDF_DATA: require("./usefull.functions").GET_PDF_DATA,
+    GET_NAMESPACE_FROM_NSP: require("./usefull.functions")
+        .GET_NAMESPACE_FROM_NSP,
+    GET_NAMESPACE_FROM_CLASSID: require("./usefull.functions")
+        .GET_NAMESPACE_FROM_CLASSID,
+    LOGGER: require("./usefull.functions").LOGGER,
+    GET_TIME: require("./usefull.functions").GET_TIME
 }
